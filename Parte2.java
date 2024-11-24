81313275bf6c1b59efd60cdd3740ace7e0aca9e3
Adriana Serpa <adriana.serpa@edu.pucrs.br>
2024-11-24 13:33:24 -0300

    Decifra e Cifra mensagem.

--- Parte2.java
+++ Parte2.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Parte2 {
 
     //  Autores: Adriana Serpa, Marcos Sanhudo
@@ -33,6 +38,39 @@ public class Parte2 {
         }
 
         return stringRevertida;
+    }
+
+    public String decifraMensagem (String mensagem, byte[] chaveGerada){
+        byte[] byteMsg = transformaEmBytes(mensagem);
+        byte[] iv = Arrays.copyOfRange(byteMsg, 0, 16);
+        byte[] mensagemCifrada = Arrays.copyOfRange(byteMsg, 16, byteMsg.length);
+
+        Cipher cifra = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        SecretKeySpec chavePrivada = new SecretKeySpec(chaveGerada, "AES");
+        cifra.init(Cipher.DECRYPT_MODE, chavePrivada, new IvParameterSpec(iv)); 
+
+        byte[] mensagemDecifrada = cifra.doFinal(mensagemCifrada);
+        return new String(mensagemDecifrada);
+    }
+
+    public String cifraMensagem(String mensagem, byte[] chaveGerada){
+        byte[] randomIv = new byte[16];
+        criarChave.getInstance("AES").generateKey().getEncoded();
+        IvParameterSpec ivParam = IvParameterSpec(randomIv);
+
+        Cipher cifra = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        SecretKeySpec chavePrivada = new SecretKeySpec(chaveGerada, "AES");
+        cifra.init(Cipher.ENCRYPT_MODE, chavePrivada); 
+
+        byte[] mensagemCifrada = cifra.doFinal(mensagem.getBytes());
+        
+        // Combinar IV e mensagem cifrada
+        byte[] mensagemFinal = new byte[randomIv.length + mensagemCifrada.length];
+        System.arraycopy(randomIv, 0, mensagemFinal, 0, randomIv.length);
+        System.arraycopy(mensagemCifrada, 0, mensagemFinal, randomIv.length, mensagemCifrada.length);
+
+        return bytesToHex(mensagemFinal);
+
     }
 
 }
